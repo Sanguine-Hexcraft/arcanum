c91ad09dbb0e326f932844f63e65fada3c0d4dcd
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2026-01-05 19:43:32 -0700

    formatting fix

--- watcher/the_watcher.py
+++ watcher/the_watcher.py
@@ -5,7 +5,7 @@ from pydub import AudioSegment     # Import pydub to play audio alerts
 import subprocess
 
 # Path to the file we want to watch.
-p = Path("/mnt/c/EQLite/Logs/eqlog_Marthus_P1999Green.txt")
+p = Path("/mnt/c/EQLite/Logs/eqlog_Sanguinne_P1999Green.txt")
 
 # The line of text we want to watch for
 phrase = "Your charm spell has worn off."
@@ -17,7 +17,7 @@ sound = AudioSegment.from_file(sound_file)
 with p.open() as f:                 # Open the log file for reading
     f.seek(0, 2)                    # Move the file pointer to the end (ignore old lines)
 
-    
+
     print("Watching for Charm Break")
 
     while True:                     # Run forver and ever and ever...
@@ -26,7 +26,7 @@ with p.open() as f:                 # Open the log file for reading
         if not line:                # If no new line is available yet
             time.sleep(0.2)         # Pause briefly to avoid busy-waiting
             continue                # Go back to the top of the loop
-        
+
         if phrase.lower() in line.lower():
             print("MATCH:", line.rstrip())
 
